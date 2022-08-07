5abdfa004664983aa1979218d437fb2704e4d1a5
Abanoub321 <abanoubmilad12@gmail.com>
2022-08-07 15:40:13 +0200

    Feat: dyana and wazayf constants

--- pages/constants/index.tsx
+++ pages/constants/index.tsx
@@ -3,56 +3,74 @@ import React from "react";
 import SideBar from "../../components/sideBar";
 
 const Home = (props: any) => {
-  const router = useRouter();
-  return (
-    <div>
-      <div className="flex flex-row items-center justify-around m-12 font-display basis-5/6 mr-80">
-        <button
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => {
-            router.push("/constants/manateq");
-          }}
-        >
-          المناطق
-        </button>
-        <button
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => {
-            router.push("/constants/mohafazat");
-          }}
-        >
-          المحافظات
-        </button>
-        <button
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => {
-            router.push("/constants/agazat");
-          }}
-        >
-          الاجازات
-        </button>
+    const router = useRouter();
+    return (
+        <div>
+            <div className="flex flex-row items-center justify-around m-12 font-display basis-5/6 mr-80">
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/manateq");
+                    }}
+                >
+                    المناطق
+                </button>
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/mohafazat");
+                    }}
+                >
+                    المحافظات
+                </button>
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/agazat");
+                    }}
+                >
+                    الاجازات
+                </button>
 
-        <button
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => {
-            router.push("/constants/hwafezReasons");
-          }}
-        >
-          اسباب الحوافز
-        </button>
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/hwafezReasons");
+                    }}
+                >
+                    اسباب الحوافز
+                </button>
 
-        <button
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => {
-            router.push("/constants/khasmReasons");
-          }}
-        >
-          اسباب الخصومات
-        </button>
-      </div>
-      <SideBar pageName="const" />
-    </div>
-  );
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/khasmReasons");
+                    }}
+                >
+                    اسباب الخصومات
+                </button>
+            </div>
+            <div className="flex flex-row items-center justify-around m-12 font-display basis-5/6 mr-80">
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/dyana");
+                    }}
+                >
+                    الديانات
+                </button>
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/wazefa");
+                    }}
+                >
+                    الوظائف
+                </button>
+            </div>
+            <SideBar pageName="const" />
+        </div>
+    );
 };
 
 export default Home;
