ab6c114e4e995a54df302b3e02e8c662392b2508
Abanoub321 <abanoubmilad12@gmail.com>
2022-08-09 12:49:44 +0200

    Feat: agazat weekly constants

--- pages/constants/index.tsx
+++ pages/constants/index.tsx
@@ -75,6 +75,14 @@ const Home = (props: any) => {
                 >
                     الجنس
                 </button>
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/weeklyvacation");
+                    }}
+                >
+                    الاجازات الاسبوعية
+                </button>
             </div>
             <SideBar pageName="const" />
         </div>
