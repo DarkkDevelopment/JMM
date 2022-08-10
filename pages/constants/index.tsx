c905e58dfcff5763e2389ccb14f73d0dc8c7711c
Abanoub321 <abanoubmilad12@gmail.com>
2022-08-10 18:16:43 +0200

    Feat: System Constants

--- pages/constants/index.tsx
+++ pages/constants/index.tsx
@@ -92,6 +92,16 @@ const Home = (props: any) => {
                     الاجازات الرسمية
                 </button>
             </div>
+            <div className="flex flex-row items-center justify-around m-12 font-display basis-5/6 mr-80">
+                <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                    onClick={() => {
+                        router.push("/constants/system");
+                    }}
+                >
+                    الثوابت
+                </button>
+            </div>
             <SideBar pageName="const" />
         </div>
     );
