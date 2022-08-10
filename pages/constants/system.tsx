66600c12804d1ef19cbb0fb7be5f8437c0d537ea
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-11 01:08:31 +0200

    return to basics

--- pages/constants/system.tsx
+++ pages/constants/system.tsx
@@ -6,95 +6,99 @@ import { SystemConstantsRow } from "../../components/SystemConstantsRow";
 import { InferGetServerSidePropsType } from "next";
 import { NEXT_PUBLIC_HOST } from "../../config/config";
 
-const System = (
-  props: InferGetServerSidePropsType<typeof getServerSideProps>
-) => {
-  const ourStartHour = props.Time.startHour;
-  const ourEndHour = props.Time.endHour;
-  const ourHafezExtraHourRatio = props.hafezAndkhasmRatios.hafezExtraHourRatio;
-  const ourKhasmLateHourRatio = props.hafezAndkhasmRatios.khasmLateHourRatio;
-  const ourHafezExtraDayRatio = props.hafezAndkhasmRatios.hafezExtraDayRatio;
-  const ourKhasmLateDayRatio = props.hafezAndkhasmRatios.khasmLateDayRatio;
-  const ourPersonInsurancePercentage =
-    props.InsurancePercentage.personInsurancePercentage;
-  const ourCompanyInsurancePercentage =
-    props.InsurancePercentage.companyInsurancePercentage;
-  const ourLoanPercentage = props.loanPercentage.loanPercentage;
+const System = () => {
+  // const ourStartHour = props.Time.startHour;
+  // const ourEndHour = props.Time.endHour;
+  // const ourHafezExtraHourRatio = props.hafezAndkhasmRatios.hafezExtraHourRatio;
+  // const ourKhasmLateHourRatio = props.hafezAndkhasmRatios.khasmLateHourRatio;
+  // const ourHafezExtraDayRatio = props.hafezAndkhasmRatios.hafezExtraDayRatio;
+  // const ourKhasmLateDayRatio = props.hafezAndkhasmRatios.khasmLateDayRatio;
+  // const ourPersonInsurancePercentage =
+  //   props.InsurancePercentage.personInsurancePercentage;
+  // const ourCompanyInsurancePercentage =
+  //   props.InsurancePercentage.companyInsurancePercentage;
+  // const ourLoanPercentage = props.loanPercentage.loanPercentage;
 
-  console.log(props);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "/api/lookupsData/getDataFromLookups/getConstants"
+      );
+      const loanPercentageResponse = await axios.get(
+        "/api/lookupsData/getDataFromLookups/globalValues"
+      );
+
+      if (loanPercentageResponse !== null) {
+        setIsLoanPercentageNew(false);
+      }
+
+      if (
+        response.data.HafezExtraDayRatio !== null &&
+        response.data.HafezExtraHourRatio !== null &&
+        response.data.KhasmLateDayRatio !== null &&
+        response.data.KhasmLateHourRatio !== null
+      ) {
+        setIsHafezAndKhasmNew(false);
+      } else {
+        setIsHafezAndKhasmNew(true);
+      }
+      if (
+        response.data.PersonInsurancePercentage !== 0 &&
+        response.data.SherkaInsurancePercentage !== 0
+      ) {
+        setIsInsuranceNew(false);
+      } else {
+        setIsInsuranceNew(true);
+      }
+
+      const { startHour, endHour } = response.data;
+      if (startHour !== "" && endHour !== "") {
+        let newStartHour = response.data.startHour.split("T")[1].slice(0, 5);
+        let newEndHour = response.data.endHour.split("T")[1].slice(0, 5);
+        setStartHour(newStartHour);
+        setEndHour(newEndHour);
+        setIsTimeNew(false);
+      } else {
+        setIsTimeNew(true);
+      }
+      // setIsTimeNew(true);
+      setLoanPercentage(loanPercentageResponse.data.Value);
+      setHafezExtraDayRatio(response.data.HafezExtraDayRatio);
+      setHafezExtraHourRatio(response.data.HafezExtraHourRatio);
+      setKhasmLateDayRatio(response.data.KhasmLateDayRatio);
+      setKhasmLateHourRatio(response.data.KhasmLateHourRatio);
+      setPersonInsurancePercentage(response.data.PersonInsurancePercentage);
+      setCompanyInsurancePercentage(response.data.SherkaInsurancePercentage);
+    };
+    fetchData();
+  }, []);
 
   const [hafezExtraDayRatio, setHafezExtraDayRatio] = useState<string | number>(
-    ourHafezExtraDayRatio
+    0
   );
   const [hafezExtraHourRatio, setHafezExtraHourRatio] = useState<
     string | number
-  >(ourHafezExtraHourRatio);
+  >(0);
   const [khasmLateDayRatio, setKhasmLateDayRatio] = useState<string | number>(
-    ourKhasmLateDayRatio
+    0
   );
   const [khasmLateHourRatio, setKhasmLateHourRatio] = useState<string | number>(
-    ourKhasmLateHourRatio
+    0
   );
   const [personInsurancePercentage, setPersonInsurancePercentage] = useState<
     string | number
-  >(ourPersonInsurancePercentage);
+  >(0);
   const [companyInsurancePercentage, setCompanyInsurancePercentage] = useState<
     string | number
-  >(ourCompanyInsurancePercentage);
-  const [startHour, setStartHour] = useState<string>(ourStartHour);
-  const [endHour, setEndHour] = useState<string>(ourEndHour);
+  >(0);
+  const [startHour, setStartHour] = useState<string>("09:00");
+  const [endHour, setEndHour] = useState<string>("17:00");
 
-  const [loanPercentage, setLoanPercentage] = useState<string | number>(
-    ourLoanPercentage
-  );
-  const [isHafezAndKhasmNew, setIsHafezAndKhasmNew] = useState(false);
-  const [isInsuranceNew, setIsInsuranceNew] = useState(false);
-  const [isTimeNew, setIsTimeNew] = useState(false);
-  const [isLoanPercentageNew, setIsLoanPercentageNew] = useState(false);
-
-  useEffect(() => {
-    const checkForValues = () => {
-      if (ourStartHour === "" && ourEndHour === "") {
-        setIsTimeNew(true);
-      } else {
-        setIsTimeNew(false);
-      }
-      if (
-        ourHafezExtraDayRatio === 0 &&
-        ourHafezExtraHourRatio === 0 &&
-        ourKhasmLateDayRatio === 0 &&
-        ourKhasmLateHourRatio === 0
-      ) {
-        setIsHafezAndKhasmNew(true);
-      } else {
-        setIsHafezAndKhasmNew(false);
-      }
-      if (
-        ourPersonInsurancePercentage === 0 &&
-        ourCompanyInsurancePercentage === 0
-      ) {
-        setIsInsuranceNew(true);
-      } else {
-        setIsInsuranceNew(false);
-      }
-      if (ourLoanPercentage === 0) {
-        setIsLoanPercentageNew(true);
-      } else {
-        setIsLoanPercentageNew(false);
-      }
-    };
-    checkForValues();
-  }, [
-    ourCompanyInsurancePercentage,
-    ourEndHour,
-    ourHafezExtraDayRatio,
-    ourHafezExtraHourRatio,
-    ourKhasmLateDayRatio,
-    ourKhasmLateHourRatio,
-    ourLoanPercentage,
-    ourPersonInsurancePercentage,
-    ourStartHour,
-  ]);
+  const [loanPercentage, setLoanPercentage] = useState<string | number>(0);
+  const [isHafezAndKhasmNew, setIsHafezAndKhasmNew] = useState(true);
+  const [isInsuranceNew, setIsInsuranceNew] = useState(true);
+  const [isTimeNew, setIsTimeNew] = useState(true);
+  const [isLoanPercentageNew, setIsLoanPercentageNew] = useState(true);
 
   const onHafezDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHafezExtraDayRatio(e.target.value);
@@ -318,100 +322,46 @@ const System = (
   );
 };
 
-// useEffect(() => {
-//   const fetchData = async () => {
-//     const response = await axios.get(
-//       "/api/lookupsData/getDataFromLookups/getConstants"
-//     );
-//     const loanPercentageResponse = await axios.get(
-//       "/api/lookupsData/getDataFromLookups/globalValues"
-//     );
+// export async function getServerSideProps(context: any) {
+//   const getAllConstants = await fetch(
+//     `https://jmm-systems.vercel.app/api/lookupsData/getDataFromLookups/getConstants`
+//   );
+//   const getAllLoanPercentage = await fetch(
+//     `https://jmm-systems.vercel.app/api/lookupsData/getDataFromLookups/globalValues`
+//   );
 
-//     if (loanPercentageResponse !== null) {
-//       setIsLoanPercentageNew(false);
-//     }
+//   const Constants = await getAllConstants.json();
+//   const LoanPercentage = await getAllLoanPercentage.json();
 
-//     if (
-//       response.data.HafezExtraDayRatio !== null &&
-//       response.data.HafezExtraHourRatio !== null &&
-//       response.data.KhasmLateDayRatio !== null &&
-//       response.data.KhasmLateHourRatio !== null
-//     ) {
-//       setIsHafezAndKhasmNew(false);
-//     } else {
-//       setIsHafezAndKhasmNew(true);
-//     }
-//     if (
-//       response.data.PersonInsurancePercentage !== 0 &&
-//       response.data.SherkaInsurancePercentage !== 0
-//     ) {
-//       setIsInsuranceNew(false);
-//     } else {
-//       setIsInsuranceNew(true);
-//     }
-
-//     const { startHour, endHour } = response.data;
-//     if (startHour !== "" && endHour !== "") {
-//       let newStartHour = response.data.startHour.split("T")[1].slice(0, 5);
-//       let newEndHour = response.data.endHour.split("T")[1].slice(0, 5);
-//       setStartHour(newStartHour);
-//       setEndHour(newEndHour);
-//       setIsTimeNew(false);
-//     } else {
-//       setIsTimeNew(true);
-//     }
-//     // setIsTimeNew(true);
-//     setLoanPercentage(loanPercentageResponse.data.Value);
-//     setHafezExtraDayRatio(response.data.HafezExtraDayRatio);
-//     setHafezExtraHourRatio(response.data.HafezExtraHourRatio);
-//     setKhasmLateDayRatio(response.data.KhasmLateDayRatio);
-//     setKhasmLateHourRatio(response.data.KhasmLateHourRatio);
-//     setPersonInsurancePercentage(response.data.PersonInsurancePercentage);
-//     setCompanyInsurancePercentage(response.data.SherkaInsurancePercentage);
+//   const hafezAndkhasmRatios = {
+//     hafezExtraDayRatio: Constants.data.HafezExtraDayRatio,
+//     hafezExtraHourRatio: Constants.data.HafezExtraHourRatio,
+//     khasmLateDayRatio: Constants.data.KhasmLateDayRatio,
+//     khasmLateHourRatio: Constants.data.KhasmLateHourRatio,
 //   };
-//   fetchData();
-// }, []);
 
-export async function getServerSideProps(context: any) {
-  const getAllConstants = await fetch(
-    `https://jmm-systems.vercel.app/api/lookupsData/getDataFromLookups/getConstants`
-  );
-  const getAllLoanPercentage = await fetch(
-    `https://jmm-systems.vercel.app/api/lookupsData/getDataFromLookups/globalValues`
-  );
+//   const InsurancePercentage = {
+//     personInsurancePercentage: Constants.data.PersonInsurancePercentage,
+//     companyInsurancePercentage: Constants.data.SherkaInsurancePercentage,
+//   };
 
-  const Constants = await getAllConstants.json();
-  const LoanPercentage = await getAllLoanPercentage.json();
+//   const Time = {
+//     startHour: Constants.data.startHour.split("T")[1].slice(0, 5),
+//     endHour: Constants.data.endHour.split("T")[1].slice(0, 5),
+//   };
 
-  const hafezAndkhasmRatios = {
-    hafezExtraDayRatio: Constants.data.HafezExtraDayRatio,
-    hafezExtraHourRatio: Constants.data.HafezExtraHourRatio,
-    khasmLateDayRatio: Constants.data.KhasmLateDayRatio,
-    khasmLateHourRatio: Constants.data.KhasmLateHourRatio,
-  };
+//   const loanPercentage = {
+//     loanPercentage: LoanPercentage.data,
+//   };
 
-  const InsurancePercentage = {
-    personInsurancePercentage: Constants.data.PersonInsurancePercentage,
-    companyInsurancePercentage: Constants.data.SherkaInsurancePercentage,
-  };
-
-  const Time = {
-    startHour: Constants.data.startHour.split("T")[1].slice(0, 5),
-    endHour: Constants.data.endHour.split("T")[1].slice(0, 5),
-  };
-
-  const loanPercentage = {
-    loanPercentage: LoanPercentage.data,
-  };
-
-  return {
-    props: {
-      hafezAndkhasmRatios,
-      InsurancePercentage,
-      Time,
-      loanPercentage,
-    },
-  };
-}
+//   return {
+//     props: {
+//       hafezAndkhasmRatios,
+//       InsurancePercentage,
+//       Time,
+//       loanPercentage,
+//     },
+//   };
+// }
 
 export default System;
