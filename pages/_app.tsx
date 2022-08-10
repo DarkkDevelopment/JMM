40d5de914d4d49246b6e809657568e31b9aa95d2
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-10 13:45:08 +0200

    update the index and app files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className="font-display">
+        <Head>
+          <title>JMM</title>
+          <meta
+            name="description"
+            content="this is an erp-system Website fully customizable"
+          />
+          <link rel="icon" href="/logo.png" />
+        </Head>
+      </div>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,6 @@ import Logo from "../public/images/logo.png";
 const Home: NextPage = (props) => {
   return (
     <>
-      <div className="font-display">
-        <Head>
-          <title>JMM</title>
-          <meta
-            name="description"
-            content="this is an erp-system Website fully customizable"
-          />
-          <link rel="icon" href="/logo.png" />
-        </Head>
-      </div>
       <div className="flex flex-col">
         <div className="flex flex-row justify-evenly mt-52">
           <Image src={Logo} width={300} height={300} alt="logo" />
