ab767f510379378aa2db2ff8c99c2a6952a6e72d
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-13 13:39:52 +0200

    adding get Manateq by Mohafza and insert new Manteqa endpoints

--- pages/api/lookupsData/insertDataIntoLookups/manteqa.ts
+++ pages/api/lookupsData/insertDataIntoLookups/manteqa.ts
@@ -3,13 +3,18 @@ import prisma from "../../../../lib/prisma";
 
 // http://localhost:3000/api/lookupsData/insertDataIntoLookups/manteqa
 
+// todo : here you should remember that we send mohafza Id with inserting new manteqa to make manateq related to mohafzat
+
 const createNewManteqaEndpoint = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  const mohafzaId = req.body.mohafzaId;
   const manteqaName = req.body.manteqaName;
+
   const inertNewManteqa = await prisma.manteqaLookup.create({
     data: {
+      Manteqa_MohafzaID: mohafzaId,
       ManteqaName: manteqaName,
     },
   });
