7f9a518c0118371d69e9dcd4ca460f0b9462149f
Abanoub321 <abanoubmilad12@gmail.com>
2022-08-13 15:38:03 +0200

    Fix: extract get function to services

--- services/employeesServices.ts
+++ services/employeesServices.ts
@@ -1,8 +1,13 @@
-const getAllEmployees = async () => {
-  const employees = await fetch(
+import axios from "axios";
+import { ApiResponseModel } from "../models/ApiResponseModel";
+
+const getAllEmployees = async ():Promise<ApiResponseModel> => {
+  const employees = await axios.get(
     `${process.env.NEXT_PUBLIC_HOST}/api/employee/getAll`
   );
-  const employeesJson = await employees.json();
-  return employeesJson;
+  return {
+    data: employees.data,
+    success: true,
+  };
 };
 export default getAllEmployees;
