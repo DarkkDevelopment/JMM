e7a5e7b4bab3bc9948a701ac965885b6821ae6c0
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-17 20:48:47 +0200

    fix some things in Money Page

--- pages/HR/constants/system.tsx
+++ pages/HR/constants/system.tsx
@@ -6,7 +6,8 @@ import { SystemConstantsRow } from "../../../components/SystemConstantsRow";
 import { InferGetServerSidePropsType } from "next";
 import { NEXT_PUBLIC_HOST } from "../../../config/config";
 
-const System = () => {
+// @ts-ignore
+const System = (props) => {
   // const ourStartHour = props.Time.startHour;
   // const ourEndHour = props.Time.endHour;
   // const ourHafezExtraHourRatio = props.hafezAndkhasmRatios.hafezExtraHourRatio;
@@ -104,6 +105,20 @@ const System = () => {
   const [isInsuranceNew, setIsInsuranceNew] = useState(true);
   const [isTimeNew, setIsTimeNew] = useState(true);
   const [isLoanPercentageNew, setIsLoanPercentageNew] = useState(true);
+  const [elawaPercentage, setElawaPercentage] = useState<string | number>(0);
+  const [badalatPercentage, setBadalatPercentage] = useState<string | number>(
+    0
+  );
+
+  const onElawaPercentageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setElawaPercentage(e.target.value);
+  };
+
+  const onBadalatPercentageChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setBadalatPercentage(e.target.value);
+  };
 
   const onHafezDayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHafezExtraDayRatio(e.target.value);
@@ -206,10 +221,16 @@ const System = () => {
   };
 
   const editTime = async () => {
-    let startHourArr = startHour.split(":"); 
+    let startHourArr = startHour.split(":");
     let endHourArr = endHour.split(":");
-    let utcStartHour = new Date().setUTCHours(Number(startHourArr[0]), Number(startHourArr[1]));
-    let utcEndHour = new Date().setUTCHours(Number(endHourArr[0]), Number(endHourArr[1]));
+    let utcStartHour = new Date().setUTCHours(
+      Number(startHourArr[0]),
+      Number(startHourArr[1])
+    );
+    let utcEndHour = new Date().setUTCHours(
+      Number(endHourArr[0]),
+      Number(endHourArr[1])
+    );
     let newStartHour = new Date(utcStartHour);
     let newEndHour = new Date(utcEndHour);
     console.log(newEndHour);
@@ -233,15 +254,14 @@ const System = () => {
 
   return (
     <div className="flex flex-row bg-gray-100">
-      <div className="grid grid-cols-2 grid-rows-2font-display basis-5/6 ml-12 mt-6 mr-80">
-        <div className = "mx-2 my-2 rounded-3xl p-5 bg-white shadow-lg">
-            <SystemConstantsRow
-              title="نسبة معامل الاضافة اليومية"
-              value={hafezExtraDayRatio}
-              onChange={onHafezDayChange}
-            />
-          
-          
+      <div className="grid grid-cols-2 mt-6 ml-12 grid-rows-2font-display basis-5/6 mr-80">
+        <div className="p-5 mx-2 my-2 bg-white shadow-lg rounded-3xl">
+          <SystemConstantsRow
+            title="نسبة معامل الاضافة اليومية"
+            value={hafezExtraDayRatio}
+            onChange={onHafezDayChange}
+          />
+
           <SystemConstantsRow
             title="نسبة معامل الساعة الاضافية"
             value={hafezExtraHourRatio}
@@ -268,7 +288,7 @@ const System = () => {
             </button>
           </div>
         </div>
-        <div className="flex flex-col mx-2 my-2 rounded-3xl p-5 bg-white shadow-lg">
+        <div className="flex flex-col p-5 mx-2 my-2 bg-white shadow-lg rounded-3xl">
           <SystemConstantsRow
             title="نسبة التأمين الشخصي"
             value={personInsurancePercentage}
@@ -279,7 +299,7 @@ const System = () => {
             value={companyInsurancePercentage}
             onChange={onCompanyInsuranceChange}
           />
-          <div className="flex flex-col flex-1 self-center justify-end">
+          <div className="flex flex-col self-center justify-end flex-1">
             <button
               className="m-3 px-4 py-2 text-center shadow appearance-none border rounded-lg w-[10vw]  text-white leading-tight focus:outline-none focus:shadow-outline hover:bg-blue-500 bg-blue-900"
               onClick={isInsuranceNew ? addInsurance : editInsurance}
@@ -288,7 +308,7 @@ const System = () => {
             </button>
           </div>
         </div>
-        <div className="flex flex-col flex-1 mx-2 my-2 rounded-3xl p-5 bg-white shadow-lg">
+        <div className="flex flex-col flex-1 p-5 mx-2 my-2 bg-white shadow-lg rounded-3xl">
           <SystemConstantsRow
             title="نسبة السلفة"
             value={loanPercentage}
@@ -297,16 +317,16 @@ const System = () => {
           {/* to be changed */}
           <SystemConstantsRow
             title="نسبة العلوات"
-            value={loanPercentage}
-            onChange={onPersonLoanPercentageChange}
+            value={elawaPercentage}
+            onChange={onElawaPercentageChange}
           />
           {/* to be changed */}
           <SystemConstantsRow
             title="نسبة البدلات"
-            value={loanPercentage}
-            onChange={onPersonLoanPercentageChange}
+            value={badalatPercentage}
+            onChange={onBadalatPercentageChange}
           />
-          <div className="flex flex-col flex-1 self-center justify-end">
+          <div className="flex flex-col self-center justify-end flex-1">
             <button
               className="m-3 px-4 py-2 text-center shadow appearance-none border rounded-lg w-[10vw]  text-white leading-tight focus:outline-none focus:shadow-outline hover:bg-blue-500 bg-blue-900"
               onClick={
@@ -317,8 +337,8 @@ const System = () => {
             </button>
           </div>
         </div>
-        <div className = "flex flex-col mx-2 my-2 rounded-3xl p-5 bg-white shadow-lg">
-          <div className="flex flex-row items-baseline m-4 justify-between">
+        <div className="flex flex-col p-5 mx-2 my-2 bg-white shadow-lg rounded-3xl">
+          <div className="flex flex-row items-baseline justify-between m-4">
             <input
               className="px-4 py-3 text-center border border-gray-300 rounded-lg w-fit focus:outline-blue-500"
               type="time"
@@ -328,7 +348,7 @@ const System = () => {
             />
             <h1>مواعيد بدا العمل</h1>
           </div>
-          <div className="flex flex-row items-baseline m-4 justify-between">
+          <div className="flex flex-row items-baseline justify-between m-4">
             <input
               className="px-4 py-3 text-center border border-gray-300 rounded-lg w-fit focus:outline-blue-500"
               type="time"
@@ -339,7 +359,7 @@ const System = () => {
             <h1>مواعيد انتهاء العمل</h1>
           </div>
 
-          <div className="flex flex-col flex-1 self-center justify-end">
+          <div className="flex flex-col self-center justify-end flex-1">
             <button
               className="m-3 px-4 py-2 text-center shadow appearance-none border rounded-lg w-[10vw]  text-white leading-tight focus:outline-none focus:shadow-outline hover:bg-blue-500 bg-blue-900"
               onClick={isTimeNew ? addTime : editTime}
