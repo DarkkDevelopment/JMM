4f672c8ef611303d2630a795e1bcfb8ab23f0a17
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-22 21:32:26 +0200

    try to fix some error

--- services/constantsService.ts
+++ services/constantsService.ts
@@ -1,38 +1,64 @@
-
 import { ApiResponseModel } from "../models/ApiResponseModel";
 import axios from "../utils/axios";
 
 export const getMohafzatService = async (): Promise<ApiResponseModel> => {
-    const response = await axios.get('/api/lookupsData/getDataFromLookups/mohafazat');
+  const response = await axios.get(
+    "/api/lookupsData/getDataFromLookups/mohafazat"
+  );
+  if (response.data) {
     const agazat = response.data.map(
-        (item: { MohafzaID: number; MohafzaName: string }) => {
-            return { id: item.MohafzaID, name: item.MohafzaName };
-        }
+      (item: { MohafzaID: number; MohafzaName: string }) => {
+        return { id: item.MohafzaID, name: item.MohafzaName };
+      }
     );
     return {
-        data: agazat,
-        success: true,
+      data: agazat,
+      success: true,
     };
-}
+  } else {
+    return {
+      data: [],
+      success: false,
+    };
+  }
+};
 
 export const getManateqService = async (): Promise<ApiResponseModel> => {
-    const response = await axios.get('/api/lookupsData/getDataFromLookups/manateq');
-    const manateq = response.data.map((manteqa: { ManteqaID: number, ManteqaName: string }) => {
-        return { id: manteqa.ManteqaID, name: manteqa.ManteqaName }
-    });
-    return {
-        data: manateq,
-        success: true,
-    };
-}
+  const response = await axios.get(
+    "/api/lookupsData/getDataFromLookups/manateq"
+  );
+  const manateq = response.data.map(
+    (manteqa: { ManteqaID: number; ManteqaName: string }) => {
+      return { id: manteqa.ManteqaID, name: manteqa.ManteqaName };
+    }
+  );
+  return {
+    data: manateq,
+    success: true,
+  };
+};
 
-export const getManateqByMohafzaIdService = async (mohafzaId: number): Promise<ApiResponseModel> => {
-    const response = await axios.get(`/api/lookupsData/getDataFromLookups/getManateqByMohafzaID?mohafzaID=${mohafzaId}`);
-    const manateq = response.data.map((manteqa: { ManteqaID: number, ManteqaName: string, Manteqa_MohafzaID: number }) => {
-        return { id: manteqa.ManteqaID, name: manteqa.ManteqaName, mohafzaId: manteqa.Manteqa_MohafzaID }
-    });
-    return {
-        data: manateq,
-        success: true,
-    };
-}
+export const getManateqByMohafzaIdService = async (
+  mohafzaId: number
+): Promise<ApiResponseModel> => {
+  const response = await axios.get(
+    `/api/lookupsData/getDataFromLookups/getManateqByMohafzaID?mohafzaID=${mohafzaId}`
+  );
+  const manateq = response.data.map(
+    (manteqa: {
+      ManteqaID: number;
+      ManteqaName: string;
+      Manteqa_MohafzaID: number;
+    }) => {
+      return {
+        id: manteqa.ManteqaID,
+        name: manteqa.ManteqaName,
+        mohafzaId: manteqa.Manteqa_MohafzaID,
+      };
+    }
+  );
+  return {
+    data: manateq,
+    success: true,
+  };
+};
