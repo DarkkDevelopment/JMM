e19c94ccf5be1009251e0d1f68c50b17e3eae25d
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-30 01:04:42 +0200

    saving some files

--- redux/features/GhyabSlice.ts
+++ redux/features/GhyabSlice.ts
@@ -1,90 +1,88 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
-import axios from '../../utils/axios'
-import { VacationsModel } from '../../models/vacationsModel'
-import { GetAbsenceModel } from '../../models/GheyabModels'
-
-
-
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "../../utils/axios";
+import { VacationsModel } from "../../models/vacationsModel";
+import { GetAbsenceModel } from "../../models/GheyabModels";
 
 export const fetchGhyabByDate = createAsyncThunk(
-    'ghyab/fetchGhyabByDate',
-    async (date: Date) => {
-        const response = await axios.post('/api/HR_Endpoints/absence/get', {
-            date
-        })
-        const agazarResponse = await axios.post('/api/HR_Endpoints/vacations/getVacationsAtThatDay', {
-            date
-        });
-        const checkForAttendanceIfOldOrNEW = await axios({
-            method: "post",
-            url: "/api/HR_Endpoints/attendance/check",
-            data: {
-                date,
-            },
-        });
+  "ghyab/fetchGhyabByDate",
+  async (date: Date) => {
+    const response = await axios.post("/api/HR_Endpoints/absence/get", {
+      date,
+    });
+    const agazarResponse = await axios.post(
+      "/api/HR_Endpoints/vacations/getVacationsAtThatDay",
+      {
+        date,
+      }
+    );
+    const checkForAttendanceIfOldOrNEW = await axios({
+      method: "post",
+      url: "/api/HR_Endpoints/attendance/check",
+      data: {
+        date,
+      },
+    });
 
-        return {
-            date,
-            ghyab: response.data,
-            old: checkForAttendanceIfOldOrNEW.data,
-            agazat: agazarResponse.data,
-        }
-    }
-)
+    return {
+      date,
+      ghyab: response.data,
+      old: checkForAttendanceIfOldOrNEW.data,
+      agazat: agazarResponse.data,
+    };
+  }
+);
 // Define a type for the slice state
 interface AbsenceState {
-    employees: GetAbsenceModel[],
-    agazat: VacationsModel[],
-    old: boolean,
-    filterDate: Date
-    status: 'loading' | 'succeeded' | 'failed'
+  employees: GetAbsenceModel[];
+  agazat: VacationsModel[];
+  old: boolean;
+  filterDate: Date;
+  status: "loading" | "succeeded" | "failed";
 }
 
 // Define the initial state using that type
 const initialState: AbsenceState = {
-    employees: [],
-    agazat: [],
-    old: false,
-    filterDate: new Date(),
-    status: 'loading'
-}
+  employees: [],
+  agazat: [],
+  old: false,
+  filterDate: new Date(),
+  status: "loading",
+};
 
 export const ghyabSlice = createSlice({
-    name: 'absence',
-    // `createSlice` will infer the state type from the `initialState` argument
-    initialState,
-    reducers: {
-        setGhyab: (state, action: PayloadAction<{ employee: GetAbsenceModel }>) => {
-            state.employees.push(action.payload.employee)
-        },
-        removeGhyab: (state, action: PayloadAction<{ id: number }>) => {
-            state.employees = state.employees.filter(e => e.PersonCode !== action.payload.id)
-        }
+  name: "absence",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    setGhyab: (state, action: PayloadAction<{ employee: GetAbsenceModel }>) => {
+      state.employees.push(action.payload.employee);
     },
-    extraReducers: builder => {
-        builder.addCase(fetchGhyabByDate.pending, (state, action) => {
-            state.status = 'loading'
-        })
-        builder.addCase(fetchGhyabByDate.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            state.employees = action.payload.ghyab;
-            state.old = action.payload.old;
-            state.filterDate = action.payload.date;
-            state.agazat = action.payload.agazat;
-        })
-        builder.addCase(fetchGhyabByDate.rejected, (state, action) => {
-            state.status = 'failed'
-        })
-    }
-})
+    removeGhyab: (state, action: PayloadAction<{ id: number }>) => {
+      state.employees = state.employees.filter(
+        (e) => e.PersonCode !== action.payload.id
+      );
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchGhyabByDate.pending, (state, action) => {
+      state.status = "loading";
+    });
+    builder.addCase(fetchGhyabByDate.fulfilled, (state, action) => {
+      state.status = "succeeded";
+      state.employees = action.payload.ghyab;
+      state.old = action.payload.old;
+      state.filterDate = action.payload.date;
+      state.agazat = action.payload.agazat;
+    });
+    builder.addCase(fetchGhyabByDate.rejected, (state, action) => {
+      state.status = "failed";
+    });
+  },
+});
 
-
-export const {
-    setGhyab,
-    removeGhyab
-} = ghyabSlice.actions
+export const { setGhyab, removeGhyab } = ghyabSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 //export const selectAttandance = (state: RootState) => state.attendance.employees
 
-export default ghyabSlice.reducer
+export default ghyabSlice.reducer;
