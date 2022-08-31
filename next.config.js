2f021e73451dec833015f7b88ed712c2d72fbdaf
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-31 17:15:45 +0200

    some changes

--- next.config.js
+++ next.config.js
@@ -5,12 +5,12 @@ const nextConfig = {
   //   NEXT_PUBLIC_HOST: "https://jmm-system.vercel.app",
   // },
   // this is for testing not production as the master branch
-  // env: {
-  //   NEXT_PUBLIC_HOST: "https://jmm-testing-version.vercel.app",
-  // },
   env: {
-    NEXT_PUBLIC_HOST: "http://localhost:3000",
+    NEXT_PUBLIC_HOST: "https://jmm-testing-version.vercel.app",
   },
+  // env: {
+  //   NEXT_PUBLIC_HOST: "http://localhost:3000",
+  // },
 };
 
 module.exports = nextConfig;
