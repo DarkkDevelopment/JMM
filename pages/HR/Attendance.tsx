2443a7346cc374e83658238bc64c1091301a15be
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-08-31 21:01:12 +0200

    fix

--- pages/HR/Attendance.tsx
+++ pages/HR/Attendance.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import "react-toastify/dist/ReactToastify.css";
 import axios from "../../utils/axios";
 import React, { useEffect, useState } from "react";
