8aa3744e03c895437a6e39d69f19e8a0cee73e6e
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-01 14:46:34 +0200

    latest updates

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   NEXT_PUBLIC_HOST: "https://jmm-system.vercel.app",
-  // },
-  // this is for testing not production as the master branch
   env: {
-    NEXT_PUBLIC_HOST: "https://jmm-testing-version.vercel.app",
+    NEXT_PUBLIC_HOST: "https://jmm-system.vercel.app",
   },
+  // this is for testing not production as the master branch
+  // env: {
+  //   NEXT_PUBLIC_HOST: "https://jmm-testing-version.vercel.app",
+  // },
   // env: {
   //   NEXT_PUBLIC_HOST: "http://localhost:3000",
   // },
