67423889d7304920b81f2bde478f0e7c1852de47
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-06 17:56:17 +0200

    some changes in the env file

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    //NEXT_PUBLIC_HOST: "https://jmm-system.vercel.app",
-    NEXT_PUBLIC_HOST:"https://jmmdeployapp.azurewebsites.net",
-    DATABASE_URL: "postgres://jmm:darkdev_2000@jmmdepolydb.postgres.database.azure.com/postgres",
+    NEXT_PUBLIC_HOST: "https://jmm-system.vercel.app",
+    //NEXT_PUBLIC_HOST: "https://jmmdeployapp.azurewebsites.net",
+    //DATABASE_URL: "postgres://jmm:darkdev_2000@jmmdepolydb.postgres.database.azure.com/postgres",
   },
   // this is for testing not production as the master branch
   // env: {
