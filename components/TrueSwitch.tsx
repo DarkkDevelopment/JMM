de21df0b01f44f189de4dd1458ee4d608f129f79
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-09-22 10:31:06 +0200

    fix some tiny problems

---
+++ components/TrueSwitch.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TrueSwitch() {
+  return <div>TrueSwitch</div>;
+}
+
+export default TrueSwitch;
---
+++ pages/Inventory/AddProduct.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// @ts-ignore
+function AddProduct(props) {
+  return <div>AddProduct</div>;
+}
+
+export default AddProduct;
